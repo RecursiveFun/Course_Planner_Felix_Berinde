9d5b200f8587aec61743b1bfdd6e113bca48636e
Felix <50165092+FelixBerinde@users.noreply.github.com>
2023-03-21 02:18:52 -0400

    Working on WidgetEdit

---
+++ Gadget.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Course_Planner_Felix_Berinde.Models
+{
+    public class Gadget
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int InStock { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreationDate { get; set; }
+
+    }
+}
---
+++ Widget.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Course_Planner_Felix_Berinde.Models
+{
+    public class Widget
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public int GadgetId { get; set; } //Foreign key from the Gadget class/table
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int InStock { get; set; }
+        public decimal Price { get; set; }
+        public bool StartNotification { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Notes { get; set; }
+        
+    }
+}
